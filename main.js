9481388b6989d03141144d4bd661d58257dafe82
Imran Qasim <imran_qasim@hotmail.com>
2021-09-27 20:58:59 +0500

    Classic Memory Game Created using HTML, CSS and JAVASCRIPT

---
+++ main.js
@@ -0,0 +1,60 @@
+const boxes = document.querySelectorAll(".box");
+let compare = [];
+let indexes = [];
+let time = 0;
+let count = 0;
+let interval = null;
+let timeout = null;
+function init() {
+	const randNums = shuffle([1, 2, 3, 4, 5, 6, 1, 2, 3, 4, 5, 6]);
+	boxes.forEach((box, index) => {
+		box.innerText = randNums[index];
+		setTimeout(() => {
+			box.innerText = "";
+		}, 2000);
+		box.addEventListener("click", (el) => {
+			el.target.innerText = randNums[index];
+			compare.push(randNums[index]);
+			indexes.push(index);
+			if (compare.length === 2) {
+				if (compare[0] === compare[1] && indexes[0] !== indexes[1]) {
+					count += 1;
+					indexes.forEach((item) =>
+						boxes[item].setAttribute("disabled", "disabled")
+					);
+				} else {
+					indexes.forEach((item) => (boxes[item].innerText = ""));
+				}
+				compare = [];
+				indexes = [];
+			}
+			if (count === 6) {
+				boxes.forEach((box) =>
+					box.setAttribute("disabled", "disabled")
+				);
+				clearInterval(interval);
+				clearTimeout(timeout);
+			}
+		});
+	});
+}
+timeout = setTimeout(() => {
+	interval = setInterval(() => {
+		time += 1;
+		document.querySelector("#score span").innerText = time;
+	}, 1000);
+}, 1000);
+function shuffle(arr) {
+	for (let i = arr.length - 1; i > 0; i--) {
+		const j = Math.floor(Math.random() * (i + 1));
+		const temp = arr[i];
+		arr[i] = arr[j];
+		arr[j] = temp;
+	}
+	return arr;
+}
+init();
+
+document
+	.getElementById("reset")
+	.addEventListener("click", () => location.reload());
